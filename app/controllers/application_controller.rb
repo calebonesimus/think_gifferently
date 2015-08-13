cc9cb95801f0b0fd4cdf682e6f2d658cc087e4e2
Caleb Francis <calebonesimus@gmail.com>
2015-08-13 16:03:00 -0400

    Implement signup/login modals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_user_signup
+  before_action :set_user_login
 
   private
+
+  def set_user_signup
+    @user = User.new
+  end
+
+  def set_user_login
+    @user_session = UserSession.new
+  end
 
   def current_user_session
     return @current_user_session if defined?(@current_user_session)

a31b6bd7fd8253d32b4cc1df19767fb0892a6a71
Caleb Francis <calebonesimus@gmail.com>
2015-08-13 14:06:36 -0400

    Make nav partial and update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# These are all being created without users for now
+
+Gif.create(image_file: "american.gif",
+           title: "American",
+           summary: "America is awesome.")
+
+Gif.create(image_file: "building.gif",
+          title: "Cool Building",
+          summary: "Interesting illustration of a building.")
+
+Gif.create(image_file: "chaos.gif",
+           title: "Cloud Chaos",
+           summary: "This thing is nuts.")
+
+
+Gif.create(image_file: "confused.gif",
+          title: "Confused",
+          summary: "What's going on?.")
+
+Gif.create(image_file: "ew.gif",
+           title: "Ew",
+           summary: "Late Night with JF.")
+
+Gif.create(image_file: "jump_rope.gif",
+          title: "Jump Rope",
+          summary: "Creative jump roper.")
+
+Gif.create(image_file: "kutcher.gif",
+         title: "Ashton Kutcher",
+         summary: "Investor/Actor.")
+
+Gif.create(image_file: "record_cat.gif",
+          title: "DJ Cat",
+          summary: "This cat rocks.")
+
+
+Gif.create(image_file: "silence.gif",
+         title: "Silence",
+         summary: "The silver rule.")
+
+
+Gif.create(image_file: "american.gif",
+          title: "American",
+          summary: "America is awesome.")

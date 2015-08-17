7aea0ebe1f705c97f65e74a977a8b0e688c9e1c2
Caleb Francis <calebonesimus@gmail.com>
2015-08-17 15:51:45 -0400

    Only allow gifs to be uploaded

--- app/uploaders/gif_uploader.rb
+++ app/uploaders/gif_uploader.rb
@@ -16,6 +16,11 @@ class GifUploader < CarrierWave::Uploader::Base
     "gifs/"
   end
 
+  # Only allow gifs to be uploaded
+  def extension_white_list
+    %w("gif")
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:

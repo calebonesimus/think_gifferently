0565acba990ff64369ba35f6d51a23a6692996e3
Caleb Francis <calebonesimus@gmail.com>
2015-08-17 15:25:55 -0400

    Upload gifs to amazon s3

--- app/uploaders/gif_uploader.rb
+++ app/uploaders/gif_uploader.rb
@@ -13,7 +13,7 @@ class GifUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "gifs/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "gifs/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
